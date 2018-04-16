cf27d493667b2d0e7a09600be09ed7e335a37596
Daniel Eke <endanke@gmail.com>
2018-04-16 23:59:42 +0200

    - Add dummy json example for chunk query

--- src/main/java/com/app/controller/ChunkController.java
+++ src/main/java/com/app/controller/ChunkController.java
@@ -15,7 +15,29 @@ public class ChunkController {
 
     @GetMapping(CHUNK_QUERY_URL)
     public ResponseEntity queryChunk(){
-        return ResponseEntity.ok("TEST");
+        String dummyJson = "{\n" +
+                "  \"measurements\": [\n" +
+                "    {\n" +
+                "      \"type\": \"heartrate\",\n" +
+                "      \"unit\": \"bpm\",\n" +
+                "      \"value\": 128,\n" +
+                "      \"createdAt\": \"2018-04-16T21:50:43.515Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"heartrate\",\n" +
+                "      \"unit\": \"bpm\",\n" +
+                "      \"value\": 100,\n" +
+                "      \"createdAt\": \"2018-04-16T21:51:13.132Z\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"type\": \"heartrate\",\n" +
+                "      \"unit\": \"bpm\",\n" +
+                "      \"value\": 140,\n" +
+                "      \"createdAt\": \"2018-04-16T21:51:23.770Z\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+        return ResponseEntity.ok(dummyJson);
     }
 
 }
